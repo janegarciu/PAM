54668694f7affecb7afacaa6bf48e18885f928bc
Eugenia <eugenia.garciu@isa.utm.md>
2021-05-20 09:01:37 +0300

    Added unit testing class

---
+++ Lab4/TravellerApp/Traveller/TravellerTests/TravellerTests.swift
@@ -0,0 +1,33 @@
+//
+//  TravellerTests.swift
+//  TravellerTests
+//
+//  Created by Jane  on 5/20/21.
+//  Copyright © 2021 Jane's company. All rights reserved.
+//
+
+import XCTest
+
+class TravellerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
